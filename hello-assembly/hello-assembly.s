32badfbecd22be81b46c843a5d4286f071f06f0a
vzhidkov <vladimir.zhidkov@icloud.com>
2022-04-30 10:51:40 +0300

    added comments

--- hello-assembly/hello-assembly.s
+++ hello-assembly/hello-assembly.s
@@ -1,9 +1,9 @@
 .global _start
 
-_start:	mov	X0, #1
+_start:	mov	X0, #1 // stdout 
 	ldr	X1, =message
 	mov	X2, #16 
-	mov	X8, #64 // write to file system call
+	mov	X8, #64 // write to file (fd is in X0) system call
 	svc	0
 
 	mov	X0, #0
